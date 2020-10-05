370f3dc0c5fb5b65fbd3cfb53ff9f7fcc72be229
kimcumjee <51234550+kimcumjee@users.noreply.github.com>
2020-10-05 15:53:27 +0900

    Add: buttonOnclick
    기본 버튼 세팅 완료

--- SheckSheck-Kiosk/ChoicePay/ChoicePay.xaml.cs
+++ SheckSheck-Kiosk/ChoicePay/ChoicePay.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SheckSheck_Kiosk.Order;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +19,25 @@ namespace SheckSheck_Kiosk.ChoicePay
     /// <summary>
     /// Interaction logic for ChoicePay.xaml
     /// </summary>
-    public partial class ChoicePay : UserControl
+    public partial class ChoicePay : Page
     {
         public ChoicePay()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (NavigationService.CanGoForward) //앞으로 이동 가능하다면 그냥 ㄱㄱ 
+            { 
+                NavigationService.GoForward(); 
+            } 
+            else 
+            { 
+                OrderPage orderPage = new OrderPage();
+                NavigationService.Navigate(orderPage); }
+
+            
         }
     }
 }
